ed0a65dea5356e678aba9bcc3616218d2624f99d
mibers <woodrow.conran@gmail.com>
2025-03-09 23:36:46 +0000

    Added yaw and pitch, fixed setting operators, added optional world argument.

--- src/main/kotlin/dev/znci/rocket/scripting/functions/Location.kt
+++ src/main/kotlin/dev/znci/rocket/scripting/functions/Location.kt
@@ -22,59 +22,70 @@ import org.luaj.vm2.LuaTable
 import org.luaj.vm2.LuaValue
 import org.luaj.vm2.Varargs
 import org.luaj.vm2.lib.OneArgFunction
+import org.luaj.vm2.lib.TwoArgFunction
 import org.luaj.vm2.lib.VarArgFunction
 
 class LuaLocations : LuaTable() {
     init {
         set("new", object : VarArgFunction() {
             override fun invoke(args: Varargs): LuaValue {
-                if (args.narg() < 4) {
+                if (args.narg() < 3) {
                     return LuaValue.FALSE
                 }
                 val x = args.arg(1).todouble()
                 val y = args.arg(2).todouble()
                 val z = args.arg(3).todouble()
-                val worldName = args.arg(4).checkjstring()
-                return LuaLocation(x, y, z, worldName)
+                // get first world, could be replaced with getting the world specified in server.properties, I don't know if this does that
+                val world = if (args.narg() >= 4) args.arg(4).checkjstring() else Bukkit.getWorlds()[0].name
+                val yaw = if (args.narg() >= 5) args.arg(5).tofloat() else 0f
+                val pitch = if (args.narg() >= 6) args.arg(6).tofloat() else 0f
+                return LuaLocation(x, y, z, world, yaw, pitch)
             }
         })
     }
 }
 
-class LuaLocation(x: Double, y: Double, z: Double, worldName: String) : LuaTable() {
+class LuaLocation(x: Double, y: Double, z: Double, worldName: String, yaw: Float, pitch: Float) : LuaTable() {
     private var world: World? = Bukkit.getWorld(worldName)
-    private var location: Location? = world?.let { Location(it, x, y, z) }
+    private var location: Location = Location(world, x, y, z, yaw, pitch)
 
     init {
-        set("x", LuaValue.valueOf(x))
-        set("y", LuaValue.valueOf(y))
-        set("z", LuaValue.valueOf(z))
-        set("world", LuaValue.valueOf(worldName))
+        val metaTable = LuaTable().apply {
+            set("__index", object : TwoArgFunction() {
+                override fun call(table: LuaValue, key: LuaValue): LuaValue {
+                    return when (key.tojstring()) {
+                        "x" -> LuaValue.valueOf(location.x)
+                        "y" -> LuaValue.valueOf(location.y)
+                        "z" -> LuaValue.valueOf(location.z)
+                        "world" -> LuaValue.valueOf(location.world.name)
+                        "yaw" -> LuaValue.valueOf(location.yaw.toDouble())
+                        "pitch" -> LuaValue.valueOf(location.pitch.toDouble())
+                        else -> LuaValue.NIL
+                    }
+                }
+            })
+        }
+
+        this.setmetatable(metaTable)
 
         set("setX", object : OneArgFunction() {
             override fun call(value: LuaValue): LuaValue {
-                val newX = value.todouble()
-                location?.x = newX
-                set("x", LuaValue.valueOf(newX))
-                return LuaValue.valueOf(newX)
+                location.x = value.todouble()
+                return value
             }
         })
 
         set("setY", object : OneArgFunction() {
             override fun call(value: LuaValue): LuaValue {
-                val newY = value.todouble()
-                location?.y = newY
-                set("y", LuaValue.valueOf(newY))
-                return LuaValue.valueOf(newY)
+                location.y = value.todouble()
+                return value
             }
         })
 
         set("setZ", object : OneArgFunction() {
             override fun call(value: LuaValue): LuaValue {
-                val newZ = value.todouble()
-                location?.z = newZ
-                set("z", LuaValue.valueOf(newZ))
-                return LuaValue.valueOf(newZ)
+                location.z = value.todouble()
+                return value
             }
         })
 
@@ -82,9 +93,22 @@ class LuaLocation(x: Double, y: Double, z: Double, worldName: String) : LuaTable
             override fun call(value: LuaValue): LuaValue {
                 val worldArg = value.checkjstring()
                 val newWorld = Bukkit.getWorld(worldArg) ?: return LuaValue.FALSE
-                location = Location(newWorld, location?.x ?: 0.0, location?.y ?: 0.0, location?.z ?: 0.0)
-                set("world", value)
+                location = Location(newWorld, location.x, location.y, location.z, location.yaw, location.pitch)
                 return LuaValue.valueOf(newWorld.name)
+            }
+        })
+
+        set("setYaw", object : OneArgFunction() {
+            override fun call(value: LuaValue): LuaValue {
+                location.yaw = value.tofloat()
+                return LuaValue.valueOf(location.yaw.toDouble())
+            }
+        })
+
+        set("setPitch", object : OneArgFunction() {
+            override fun call(value: LuaValue): LuaValue {
+                location.pitch = value.tofloat()
+                return LuaValue.valueOf(location.pitch.toDouble())
             }
         })
     }
@@ -94,12 +118,14 @@ class LuaLocation(x: Double, y: Double, z: Double, worldName: String) : LuaTable
         val x = get("x").todouble()
         val y = get("y").todouble()
         val z = get("z").todouble()
-        return Location(world, x, y, z)
+        val yaw = get("yaw").tofloat()
+        val pitch = get("pitch").tofloat()
+        return Location(world, x, y, z, yaw, pitch)
     }
 
     companion object {
         fun fromBukkit(location: Location): LuaLocation {
-            return LuaLocation(location.x, location.y, location.z, location.world.name)
+            return LuaLocation(location.x, location.y, location.z, location.world.name, location.yaw, location.pitch)
         }
     }
 }
