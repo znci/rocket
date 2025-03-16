4894ddefa190ec4ebd968d208ef542fa437fa153
Lexî Nuutraî <info@nuutrai.com>
2025-03-16 07:23:57 +0100

    Adjust LuaEvents to use new system

--- src/main/kotlin/dev/znci/rocket/scripting/functions/Events.kt
+++ src/main/kotlin/dev/znci/rocket/scripting/functions/Events.kt
@@ -18,6 +18,7 @@ package dev.znci.rocket.scripting.functions
 import dev.znci.rocket.Rocket.Companion.INSTANCE
 import dev.znci.rocket.scripting.ScriptManager
 import dev.znci.rocket.scripting.events.EventListener
+import org.luaj.vm2.LuaFunction
 import org.luaj.vm2.LuaTable
 import org.luaj.vm2.LuaValue
 import org.luaj.vm2.lib.TwoArgFunction
@@ -28,17 +29,53 @@ class LuaEvents : LuaTable() {
             override fun call(eventName: LuaValue, callback: LuaValue): LuaValue {
                 val eventClass = EventListener.getEventByName(eventName.tojstring())
 
-                println(eventClass?.simpleName)
-
                 if (eventClass != null) {
                     if (!EventListener.SUPPORTED_EVENTS.contains(eventClass)) return LuaValue.NIL.also { println("Not contained") }
-                    EventListener.registerEvent(eventClass)
-                    ScriptManager.usedEvents[eventClass] = callback.checkfunction()
-                    INSTANCE.logger.info(ScriptManager.usedEvents.size.toString())
+
+                    if (ScriptManager.usedEvents[eventClass] == null) EventListener.registerEvent(eventClass)
+
+                    // Cache all information about the loaded script
+                    // Ensures that we can retrieve the relevant information when dealing with unloading
+
+                    if (ScriptManager.usedEvents[eventClass] == null) {
+                        ScriptManager.usedEvents[eventClass] = mutableListOf()
+                    }
+                    val function = callback.checkfunction()
+                    ScriptManager.usedEvents[eventClass]!!.add(function)
+
+                    ScriptManager.eventScript[function] = eventClass
+
+                    val fileName = getFileNameFromLuaFunction(function)
+
+                    println(fileName)
+
+                    if (ScriptManager.loadedScriptFiles[fileName] == null) {
+                        println("not set yet")
+                        ScriptManager.loadedScriptFiles[fileName] = mutableListOf()
+                    }
+                    println("adding")
+                    ScriptManager.loadedScriptFiles[fileName]!!.add(function)
                 }
 
                 return LuaValue.NIL
             }
         })
+    }
+
+    /**
+     * Scuffed stuff, let's find a better way if we can
+     * @see ScriptManager.runScript
+     */
+
+    private fun getFileNameFromLuaFunction(function: LuaFunction?): String {
+        var fileName = ""
+        var previousChar: Char? = null
+        var record = false
+        for (char in function.toString()) {
+            if ((previousChar ?: ' ') == ':' && char == ':') if (record == false)  record = true else break
+            if (record) fileName = "$fileName$char"
+            previousChar = char
+        }
+        return fileName.subSequence(1, fileName.length-1).toString()
     }
 }
