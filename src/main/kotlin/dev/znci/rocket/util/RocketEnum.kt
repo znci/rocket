fb4306c90147c30593bf67c362ac4c34347a9720
cerqiest <66092565+cerqiest@users.noreply.github.com>
2025-03-18 18:33:09 -0400

    add ability to make enums and material, worldtype, and environment enums

---
+++ src/main/kotlin/dev/znci/rocket/util/RocketEnum.kt
@@ -0,0 +1,37 @@
+package dev.znci.rocket.util
+
+import org.bukkit.Material
+import org.bukkit.World
+import org.bukkit.WorldType
+import org.luaj.vm2.LuaTable
+
+class RocketEnum(private val values: List<String>) {
+    fun getValues(): List<String> {
+        return values
+    }
+
+    fun isValid(key: String): Boolean {
+        return values.contains(key)
+    }
+
+    fun getLuaTable(): LuaTable {
+        val table = LuaTable()
+
+        values.forEach() { value ->
+            table.set(value, value)
+        }
+
+        return table
+    }
+}
+
+fun <T : Enum<T>> enumToStringList(enumClass: Class<T>): List<String> {
+    return enumClass.enumConstants.map { it.name }
+}
+
+
+data object RocketEnums {
+    val RocketMaterial = RocketEnum(enumToStringList(Material::class.java))
+    val RocketWorldType = RocketEnum(enumToStringList(WorldType::class.java))
+    val RocketEnvironment = RocketEnum(enumToStringList(World.Environment::class.java))
+}
