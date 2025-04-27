0a8cdab2921c5b98c18952f5c6340060acf8a447
zNotChill <znotchill@znci.dev>
2025-04-27 04:19:54 +0100

    Add more math functions (with some bugs)
    Signed-off-by: zNotChill <znotchill@znci.dev>

--- src/main/kotlin/dev/znci/rocket/scripting/globals/tables/Math.kt
+++ src/main/kotlin/dev/znci/rocket/scripting/globals/tables/Math.kt
@@ -6,7 +6,6 @@ import dev.znci.twine.TwineNative
 import dev.znci.twine.annotations.TwineNativeFunction
 import dev.znci.twine.annotations.TwineOverload
 import java.lang.Math
-import kotlin.math.round
 import kotlin.math.roundToInt
 
 @Global
@@ -28,11 +27,148 @@ class Math : TwineNative("math") {
     }
 
     @TwineNativeFunction
-    fun randomInt(min: Int, max: Int): Double {
+    @TwineOverload
+    fun randomInt(min: Double, max: Double): Int {
         if (min >= max) {
             throw RocketError("min value should be less than max value")
         }
 
-        return (min + (max - min) * Math.random()).roundToInt().toDouble()
+        return (min + (max - min) * Math.random()).roundToInt()
+    }
+
+    @TwineNativeFunction
+    @TwineOverload
+    fun randomInt(): Int {
+        return randomInt(0.0, 1.0)
+    }
+
+    @TwineNativeFunction
+    fun cos(x: Double): Double {
+        return kotlin.math.cos(x)
+    }
+
+    @TwineNativeFunction
+    fun sin(x: Double): Double {
+        return kotlin.math.sin(x)
+    }
+
+    @TwineNativeFunction
+    fun tan(x: Double): Double {
+        return kotlin.math.tan(x)
+    }
+
+    @TwineNativeFunction
+    fun acos(x: Double): Double {
+        return kotlin.math.acos(x)
+    }
+
+    @TwineNativeFunction
+    fun asin(x: Double): Double {
+        return kotlin.math.asin(x)
+    }
+
+    @TwineNativeFunction
+    fun atan(x: Double): Double {
+        return kotlin.math.atan(x)
+    }
+
+    @TwineNativeFunction
+    fun atan2(y: Double, x: Double): Double {
+        return kotlin.math.atan2(y, x)
+    }
+
+    @TwineNativeFunction
+    fun sqrt(x: Double): Double {
+        return kotlin.math.sqrt(x)
+    }
+
+    @TwineNativeFunction
+    fun ceil(x: Double): Double {
+        return kotlin.math.ceil(x)
+    }
+
+    @TwineNativeFunction
+    @TwineOverload
+    fun floor(x: Double): Double {
+        return kotlin.math.floor(x)
+    }
+
+    @TwineNativeFunction
+    @TwineOverload
+    fun floor(vector3: LuaVector3): LuaVector3 {
+        return LuaVector3(
+            kotlin.math.floor(vector3.xProperty),
+            kotlin.math.floor(vector3.yProperty),
+            kotlin.math.floor(vector3.zProperty)
+        )
+    }
+
+    @TwineNativeFunction
+    @TwineOverload
+    fun abs(x: Double): Double {
+        return kotlin.math.abs(x)
+    }
+
+    @TwineNativeFunction
+    @TwineOverload
+    fun abs(vector3: LuaVector3): LuaVector3 {
+        return LuaVector3(
+            kotlin.math.abs(vector3.xProperty),
+            kotlin.math.abs(vector3.yProperty),
+            kotlin.math.abs(vector3.zProperty)
+        )
+    }
+
+//    @TwineNativeFunction
+//    @TwineOverload
+//    fun max(vararg values: Double): Double {
+//        return values.maxOrNull() ?: 0.0
+//    }
+
+    // TODO: Add overload for max with vararg
+    // TODO: Fix max and min
+    @TwineNativeFunction
+    @TwineOverload
+    fun max(vector3: LuaVector3): Double {
+        return kotlin.math.max(
+            kotlin.math.max(vector3.xProperty, vector3.yProperty),
+            vector3.zProperty
+        )
+    }
+
+//    @TwineNativeFunction
+//    @TwineOverload
+//    fun min(vararg values: Double): Double {
+//        return values.minOrNull() ?: 0.0
+//    }
+
+    @TwineNativeFunction
+    @TwineOverload
+    fun min(vector3: LuaVector3): Double {
+        return kotlin.math.min(
+            kotlin.math.min(vector3.xProperty, vector3.yProperty),
+            vector3.zProperty
+        )
+    }
+
+    @TwineNativeFunction
+    @TwineOverload
+    fun round(x: Double): Double {
+        return kotlin.math.round(x)
+    }
+
+    @TwineNativeFunction
+    @TwineOverload
+    fun round(vector3: LuaVector3): LuaVector3 {
+        return LuaVector3(
+            kotlin.math.round(vector3.xProperty),
+            kotlin.math.round(vector3.yProperty),
+            kotlin.math.round(vector3.zProperty)
+        )
+    }
+
+    @TwineNativeFunction
+    fun toDegrees(radians: Double): Double {
+        return Math.toDegrees(radians)
     }
 }
